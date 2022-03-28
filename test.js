5056bb2121a10455f6f4b2e0ac43218eade0ad47
korasin <mchemutai232@gmail.com>
2022-03-28 13:07:09 +0300

    Add js file

---
+++ test.js
@@ -0,0 +1,20 @@
+function sum(a,b){
+    return a + b
+}
+function subtract(c,d){
+    return c - d
+}
+function divide(m,f){
+    return m / f
+}
+function mult(p,q){
+    return p * q
+}
+function multipapa(a,b,sum){
+    return a*b*(sum)
+}
+console.log(sum(2,3))
+console.log(subtract(6,4))
+console.log(divide(14,7))
+console.log(mult(9,3))
+console.log(multipapa(2,4,(2,4)))
